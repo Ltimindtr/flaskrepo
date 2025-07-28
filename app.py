216258d3448022500d13d291a920509c41067e16
v-nshruthi <v-snambissan@microsoft.com>
2025-07-28 18:55:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def counter():
     return 'Hello Python!!!'
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
-if __name__ == '__main__':
-    app.run(debug=True)
